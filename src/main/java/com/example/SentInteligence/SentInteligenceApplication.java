bf79f31cf6e34d494eb0b929c940fa9c5f4b8e6d
KouHegde <koushik.shripad.hegde@gmail.com>
2024-12-04 15:18:59 +0530

    Adding new project

---
+++ src/main/java/com/example/SentInteligence/SentInteligenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SentInteligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentInteligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentInteligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SentInteligence/SentInteligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SentInteligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentInteligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
