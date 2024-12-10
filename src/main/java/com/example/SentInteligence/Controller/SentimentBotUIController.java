be5ff0783459d1a5bbad36957fc35b9f5c3f70ed
ambakuma <ambakuma@cisco.com>
2024-12-10 12:32:13 +0530

    Added Code for Retrieving Multiple Conv Id's

--- src/main/java/com/example/SentInteligence/Controller/SentimentBotUIController.java
+++ src/main/java/com/example/SentInteligence/Controller/SentimentBotUIController.java
@@ -4,21 +4,30 @@ import com.example.SentInteligence.Model.Request.RequestWrapper;
 import com.example.SentInteligence.Model.Request.TranscriptDetail;
 import com.example.SentInteligence.Model.Response.ConversationSentiment;
 import com.example.SentInteligence.Model.Response.ElasticSearchResponse;
+import com.example.SentInteligence.Model.Response.ConvosResponse;
 import com.example.SentInteligence.Model.Response.ResponseWrapper;
+import com.example.SentInteligence.Utils.ApplicationPropertiesUtils;
 import com.example.SentInteligence.Service.SentimentAnalysisService;
+import com.example.SentInteligence.Utils.JsonUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/sentiment/")
 public class SentimentBotUIController {
+
     private final SentimentAnalysisService<ElasticSearchResponse, ConversationSentiment> sentimentAnalysisService;
+    private final ApplicationPropertiesUtils applicationPropertiesUtils;
 
-
-    public SentimentBotUIController(SentimentAnalysisService<ElasticSearchResponse, ConversationSentiment> sentimentAnalysisService) {
+    public SentimentBotUIController(SentimentAnalysisService<ElasticSearchResponse, ConversationSentiment> sentimentAnalysisService, ApplicationPropertiesUtils applicationPropertiesUtils) {
         this.sentimentAnalysisService = sentimentAnalysisService;
+        this.applicationPropertiesUtils = applicationPropertiesUtils;
     }
 
     @PostMapping("/conversation/{convId}/update-ui")
@@ -33,5 +42,13 @@ public class SentimentBotUIController {
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    @GetMapping("/convos")
+    public ConvosResponse getOrgConvIds() throws JsonProcessingException {
+        String orgConvIdJson = applicationPropertiesUtils.getPropertyValue("org_convId");
+        System.out.println(orgConvIdJson);
+        return applicationPropertiesUtils.getOrgConvIds(orgConvIdJson);
+
     }
 }
---
+++ src/main/java/com/example/SentInteligence/Model/Response/ConvosResponse.java
@@ -0,0 +1,22 @@
+package com.example.SentInteligence.Model.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConvosResponse {
+    private DataObject data;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class DataObject {
+        private String orgId;
+        private List<String> conversationsIds;
+    }
+}
